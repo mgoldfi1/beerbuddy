3bacd1976847f7997e45503610e0370385eed777
mgoldfi1 <mgoldfi1@gmail.com>
2018-12-15 23:36:17 -0800

    workig markers, but need to set different map center on search

--- beerbuddy/src/components/breweryList.js
+++ beerbuddy/src/components/breweryList.js
@@ -9,31 +9,19 @@ export default class BreweryList extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {breweries: []}
     }
 
-    componentWillMount() {
-        fetch('/api/breweries')
-        .then(res => res.json())
-        .then(json => this.setState({breweries: json.breweries.sort(
-            function(a, b) {
-                var nameA = a.name.toUpperCase();
-                var nameB = b.name.toUpperCase();
-                return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
-            }
-        )}))
-    }
+
 
     render() {
-        {console.log(this.state.breweries)}
         return (
             <div>
                 <strong>Click on a brewery below to visit its page:</strong>
                 <Grid className="list">
-                {this.state.breweries.map(
-                    brewery => {
+                {this.props.breweries.map(
+                    (brewery,i) => {
                         return (
-                        <Cell col={2}>
+                        <Cell key={i} col={2}>
                         <div className="breweryCell"><a href="#">{brewery.name}</a></div>
                         </Cell>
                         )
--- beerbuddy/src/components/map.js
+++ beerbuddy/src/components/map.js
@@ -25,25 +25,69 @@ const styles = theme => ({
     constructor(props) {
         super(props);
         this.state = {
-            lat: 0,
-            lng: 0,
+            lat: 39.8333333,
+            lng: -98.585522,
             location: '',
-            isOpen: false
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+            breweries: [],
+            zoom: 5
         }
     }
 
     handleSearch = () => {
+        if (this.state.location){
         fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${this.state.location}&key=${apiKey}`)
         .then(res => res.json())
-        .then(json => {this.setState({lat: json.results[0].geometry.location.lat, lng: json.results[0].geometry.location.lng })})
+        .then(json => {this.setState({lat: json.results[0].geometry.location.lat, lng: json.results[0].geometry.location.lng, zoom: 10 })})
+        }
     }
 
+    onMarkerClick = (props, marker, e) => {
+       console.log(marker);
+       console.log(props);
+        this.setState({
+             selectedPlace: props,
+             activeMarker: marker,
+             showingInfoWindow: true
+    });
+}
+
+    onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+        }
+    };
 
     componentWillMount() {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(position => {
               this.setState({lat: position.coords.latitude, lng: position.coords.longitude }) })
             }
+            fetch('/api/breweries')
+            .then(res => res.json())
+            .then(json => this.setState({breweries: json.breweries.sort(
+                function(a, b) {
+                    var nameA = a.name.toUpperCase();
+                    var nameB = b.name.toUpperCase();
+                    return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
+                }
+            )}))
+    }
+
+    renderMarkers = () => {
+        return (
+            this.state.breweries.map(brewery => {
+                return (
+                    <Marker onClick={this.onMarkerClick} position={{lat: brewery.latitude, lng: brewery.longitude}} title={brewery.name} name={brewery.name} website={brewery.website}>
+                    </Marker>
+                )
+            })
+        )
     }
 
     renderMap = () => {
@@ -57,37 +101,40 @@ const styles = theme => ({
                 name="address"
                 margin="normal"
                 variant="outlined"
-                style={{height: 40, margin: 5}}
+                style={{height: 40, margin: 7}}
                 onChange={(event) => this.setState({location: event.target.value})}
             />
-            <Button variant="contained" onClick={this.handleSearch}style={{marginTop: 5}}color="primary">
+            <Button variant="contained" onClick={this.handleSearch}style={{marginTop: 8}}color="primary">
                 Find Breweries
             </Button>
             <Grid>
             <Cell col={12}>
             <div className="container">
             <Map
+            onClick={this.onMapClicked}
             google={this.props.google}
-            zoom={14}
+            zoom={this.state.zoom}
             style={mapStyles}
-             center={{
+             initialCenter={{
              lat: this.state.lat,
              lng: this.state.lng
             }}
                 >
-                <Marker position={{lat: '40.6710729', lng:'-73.9988001'}} name="hello">
-                <InfoWindow onCloseClick={this.handleToggle}>
-                        <div>
-                            First Brewery
-                        </div>
-                    </InfoWindow>
-                </Marker>
+                {this.renderMarkers()}
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}>
+                <div>
+                    <p><strong>{this.state.selectedPlace.name}</strong><br/>
+                    <a href={this.state.selectedPlace.website} target="_blank">Company Website</a></p>
+                </div>
+        </InfoWindow>
             </Map>
             </div>
             </Cell>
             </Grid>
           </div>
-          <div className="list"><BreweryList/></div> 
+          <div className="list"><BreweryList breweries={this.state.breweries}/></div> 
           </div>
           )
     }
