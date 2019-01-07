cff39ab076de3ef45dd252b1701a0f994916f3da
mgoldfi1 <mgoldfi1@gmail.com>
2019-01-07 14:08:47 -0800

    beer display page

--- client/src/components/beers/beer.js
+++ client/src/components/beers/beer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Navbar from '../navbar'
-
-
+import { Grid, Cell } from 'react-mdl'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
 
 export default class Beer extends Component {
     constructor(props) {
@@ -22,7 +24,24 @@ export default class Beer extends Component {
         return (
             <React.Fragment>
             <Navbar/>
-            <div>Hello</div>
+            <Grid>
+            <Cell col={12}><h1>{this.state.beer.name}</h1></Cell>
+            <Cell col={6}>
+            <p>
+                <strong>
+                    Description:
+                </strong><br/>
+                {this.state.beer.desc}</p></Cell>
+            <Cell className="beerBlurb" col={6}>
+            <img className="beerPagePic" src={this.state.beer.label}/>
+            <div className="beerPageInfo">
+                <div><strong>ABV:</strong> {this.state.beer.abv}</div>
+                <div><strong>IBU:</strong> {this.state.beer.ibu}</div>
+                <div><strong>Style:</strong> {this.state.beer.style}</div>
+                <div><strong>Brewery Name:</strong> {this.state.beer.brewery.year}</div>
+            </div>
+            </Cell>
+            </Grid>
             </React.Fragment>
         )
     }
