dce1cea43c9be644a3c73d7ebfd785ba265deec4
learn <eric.desimone@cortland.edu>
2019-01-07 14:36:15 -0500

    created server folder, yarn dev now runs nodemon server, which defaults to index.js and runs the server on port 3001

---
+++ api/server/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const models = require('../models')
+const Beer = require('../models/beer')
+const Brewery = require('../models/brewery')
+// Set up the express app
+const app = express();
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('/api/beer/:filter/:page', (req, res) => {
+  models.Beer.findAll({
+  limit: 10,
+  offset: req.params.page*10,
+  include: [{
+    model: models.Brewery,
+    as: 'brewery'
+    }],
+  order: [
+    req.params.filter
+  ]
+})
+  .then(beer => res.send({beer}))
+}
+)
+
+app.get('/api/breweries',  (req, res) => {
+  models.Brewery.findAll({})
+  .then(breweries => res.send({breweries}))
+})
+;
+
+app.listen(3001);
+
+module.export = app;
