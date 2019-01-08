90fadc469d460e993556c63d1272f804f58da07b
mgoldfi1 <mgoldfi1@gmail.com>
2019-01-07 21:53:37 -0800

    style changes

--- client/src/components/beers/beer.js
+++ client/src/components/beers/beer.js
@@ -13,20 +13,27 @@ const Beer = ({beer}) => {
         <Grid>
         <Cell col={12}><h1>{beer.name}</h1></Cell>
         <Cell col={6}>
-        <p>
+            <p>
             <strong>
                 Description:
             </strong><br/>
-            {beer.desc}</p></Cell>
+            {beer.desc}
+            </p>
+            <div><strong>Reviews for this beer: 0</strong></div> 
+            <div><strong>Ratings for this beer: 0</strong></div> 
+            </Cell>
         <Cell className="beerBlurb" col={6}>
         <img className="beerPagePic" src={beer.label}/>
         <div className="beerPageInfo">
-            <div className="beerText"><strong>ABV:</strong> {beer.name}</div>
+            <div className="beerText"><strong>ABV:</strong> {beer.abv}%</div>
             <div className="beerText"><strong>IBU:</strong> {beer.ibu}</div>
             <div className="beerText"><strong>Style:</strong> {beer.style}</div>
             <div className="beerText"><strong>Brewery Name:</strong> {beer.brewery.name}</div>
             <div className="beerText"><strong>Brewery Website:</strong> <a href={beer.brewery.website} target="_blank">{beer.brewery.website}</a></div>
         </div>
+        </Cell>
+        <Cell col={12}>
+          <div><strong>Beers With A Similar Style</strong></div>
         </Cell>
         </Grid>
         </React.Fragment>
