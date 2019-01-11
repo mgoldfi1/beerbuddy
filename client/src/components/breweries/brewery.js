aadd21fbbea9fa453cedd30691df771d8f306276
mgoldfi1 <mgoldfi1@gmail.com>
2019-01-11 13:45:10 -0800

    barebones brewery page and info

--- client/src/components/breweries/brewery.js
+++ client/src/components/breweries/brewery.js
@@ -4,6 +4,12 @@ import { Grid, Cell } from 'react-mdl';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
 
 const Brewery = (props) => {
   const brewery = props.data;
@@ -17,8 +23,61 @@ const Brewery = (props) => {
         <React.Fragment>
           <Navbar />
           <Grid>
-            <Cell col={12}>
+            <Cell className="breweryHeader" col={12}>
               <h1>{brewery.name}</h1>
+            </Cell>
+            <div style={{width: '100vw', height: '10vh'}}/>
+            <Cell col={6}>
+            <ExpansionPanel>
+                <ExpansionPanelSummary >
+                <Typography><strong>Location</strong></Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                <Typography>
+            {brewery.region + ', ' + brewery.country}
+            </Typography>
+            </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+                <ExpansionPanelSummary >
+                <Typography ><strong>Description</strong></Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                <Typography>
+            {brewery.description}
+            </Typography>
+            </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+                <ExpansionPanelSummary >
+                <Typography ><strong>Founded In</strong></Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                <Typography>
+            {brewery.year}
+            </Typography>
+            </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+                <ExpansionPanelSummary >
+                <Typography ><strong>Brewery Website</strong></Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                <Typography>
+            <a href={brewery.website} target="_blank">{brewery.website}</a>
+            </Typography>
+            </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+                <ExpansionPanelSummary >
+                <Typography ><strong>Open to the public?</strong></Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                <Typography>
+            {brewery.openToPublic === "Y" ? "Yes":"No"}
+            </Typography>
+            </ExpansionPanelDetails>
+            </ExpansionPanel>
             </Cell>
           </Grid>
         </React.Fragment>
