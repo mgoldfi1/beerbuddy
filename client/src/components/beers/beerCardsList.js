c0f912522f248810dfa42ef004386133c4746245
learn <eric.desimone@cortland.edu>
2019-01-14 14:58:25 -0500

    refactored error handling for beer route index.js for the api, now throws an error if no beers are found, front end no longer crashes if the promise is unfulfilled

--- client/src/components/beers/beerCardsList.js
+++ client/src/components/beers/beerCardsList.js
@@ -17,22 +17,23 @@ class BeerCardsList extends Component {
       }
 
 
-  loadFunc = (page) => {
-    fetch(`/api/beer/${this.state.value}/${this.state.page}`)
-      .then(res => res.json())
-      .then(json => {
-        if (json.beers.length > 0) {
-          this.setState(currentState => ({beers: [...currentState.beers, ...json.beers], page: currentState.page + 1}) )
-        }
-    })
-  }
+  loadFunc = async (page) => {
+     const res = await fetch(`/api/beer/${this.state.value}/${this.state.page}`)
+     const body = await res.json()
+     if (res.status === 200){
+         this.setState(currentState => ({beers: [...currentState.beers, ...body.beers], page: currentState.page + 1}) )
+     } else {
+       this.setState({more: false})
+     }
+}
 
   handleChange = (event) => {
-      this.setState({beers: [], value: event.target.value, page: 0 });
+      this.setState({beers: [], value: event.target.value, page: 0, more:true });
     };
 
   mapBeers = () => {
-    return this.state.beers.map((beer,index) => <BeerCard key={index} beer={beer}/>)
+    let beers = this.state.beers
+    return  beers.length > 0 ? beers.map((beer,index) => <BeerCard key={index} beer={beer}/>) : null
   }
 
   render() {
