78c077b7ab20000e96ced862d2c7623acca3edfa
learn <eric.desimone@cortland.edu>
2019-01-15 14:42:55 -0500

    building MouseEnter listener to create a seemless horizontal scroller

--- client/src/components/breweries/breweryPage/breweryBeers.js
+++ client/src/components/breweries/breweryPage/breweryBeers.js
@@ -5,6 +5,19 @@ import HorizontalScroll from 'react-scroll-horizontal'
 
 class BreweryBeers extends Component {
 
+  // componentWillMount(){
+  //   this.getWindowWidth();
+  //   window.addEventListener('resize', this.getWindowWidth)
+  // }
+  //
+  // componentWillUnmount(){
+  //   window.removeEventListener('resize', this.getWindowWidth)
+  // }
+  //
+  // getWindowWidth = () => {
+  //   console.log(window.innerWidth)
+  // }
+
   mapBeers = () => {
     return this.props.beers.map(beer => {
          return(
@@ -16,12 +29,24 @@ class BreweryBeers extends Component {
     })
   }
 
+
+  // when mouse enters div, check size of beer link and div container
+  // if container width is not equal to the width stored in local state, setState to new width
+  // beer link width will not change depending on the resizing of the window, only set state once
+  
+  handleMouseEnter = (event) => {
+    let beerLinkWidth = event.currentTarget.children[0].children[0].children[0].offsetWidth
+    let divWidth =  event.currentTarget.offsetWidth
+    console.log(beerLinkWidth, divWidth)
+    // console.log('Mouse Horizonal Position', event.clientX)
+  }
+
 render(){
     return (
         <>
         <strong style={{fontSize: '16px'}}>Beers made by this brewery:</strong><br/>
-        <div style={{height: '300px'}}>
-        <HorizontalScroll>
+        <div style={{height: '300px'}} onMouseEnter={this.handleMouseEnter}>
+        <HorizontalScroll >
           {this.mapBeers()}
           </HorizontalScroll>
         </div>
