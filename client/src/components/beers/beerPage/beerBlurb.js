a5064d712f2322e3f0488e89afe12f58246884f9
learn <eric.desimone@cortland.edu>
2019-01-14 21:14:24 -0500

    created a beerBrewery componenet and separated a beers brewerys information from the main beerBlurb component

--- client/src/components/beers/beerPage/beerBlurb.js
+++ client/src/components/beers/beerPage/beerBlurb.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Cell } from 'react-mdl';
+import BeerBrewery from './beerBrewery'
 
 const BeerBlurb = (props) => {
   const { label, abv, ibu, style, brewery, colLength } = props
@@ -7,24 +8,10 @@ const BeerBlurb = (props) => {
     <Cell className="beer-blurb" col={colLength}>
       <img className="beer-page-pic" src={label} />
       <div className="beer-page-info">
-        <div className="beer-text">
-          <strong>ABV:</strong> {abv}%
-        </div>
-        <div className="beer-text">
-          <strong>IBU:</strong> {ibu}
-        </div>
-        <div className="beer-text">
-          <strong>Style:</strong> {style}
-        </div>
-        <div className="beer-text">
-          <strong>Brewery Name:</strong> {brewery.name}
-        </div>
-        <div className="beer-text">
-          <strong>Brewery Website:</strong>{' '}
-          <a href={brewery.website} target="_blank">
-            {brewery.website}
-          </a>
-        </div>
+          <strong>ABV:</strong> {abv}%<br/>
+          <strong>IBU:</strong> {ibu}<br/>
+          <strong>Style:</strong> {style}<br/>
+          <BeerBrewery brewery={brewery}/>
       </div>
     </Cell>
   );
