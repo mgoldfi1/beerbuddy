3510d56d79bac6accd14bf6dd113a6d1512fd354
learn <eric.desimone@cortland.edu>
2019-01-15 15:00:51 -0500

    horizonal scroll on mouse enter, sets state.center to divWith/2 and offset width based off of a beerlinks width, building onMouseMove function to scroll based off of horizonal coordinate

--- client/src/components/breweries/breweryPage/breweryBeers.js
+++ client/src/components/breweries/breweryPage/breweryBeers.js
@@ -5,6 +5,11 @@ import HorizontalScroll from 'react-scroll-horizontal'
 
 class BreweryBeers extends Component {
 
+  state = {
+    center: 0,
+    offset: 0
+  }
+
   // componentWillMount(){
   //   this.getWindowWidth();
   //   window.addEventListener('resize', this.getWindowWidth)
@@ -19,9 +24,10 @@ class BreweryBeers extends Component {
   // }
 
   mapBeers = () => {
-    return this.props.beers.map(beer => {
+    return this.props.beers.map((beer, index) => {
          return(
            <Link
+            key={index}
             to={"/beer/" + beer.id}>
             <img className="brewery-beers" src={beer.label}/>
            </Link>
@@ -33,21 +39,30 @@ class BreweryBeers extends Component {
   // when mouse enters div, check size of beer link and div container
   // if container width is not equal to the width stored in local state, setState to new width
   // beer link width will not change depending on the resizing of the window, only set state once
-  
+
   handleMouseEnter = (event) => {
-    let beerLinkWidth = event.currentTarget.children[0].children[0].children[0].offsetWidth
+    let linkWidth = event.currentTarget.children[0].children[0].children[0].offsetWidth
     let divWidth =  event.currentTarget.offsetWidth
-    console.log(beerLinkWidth, divWidth)
-    // console.log('Mouse Horizonal Position', event.clientX)
+    if (this.state.divWidth !== divWidth) {
+      this.setState({center: (divWidth/2), offset: linkWidth})
+    }
+  }
+
+  handleMouseMove = (event) => {
+    console.log('Mouse Horizonal Position', event.clientX)
   }
 
 render(){
+  console.log(this.state)
     return (
         <>
         <strong style={{fontSize: '16px'}}>Beers made by this brewery:</strong><br/>
-        <div style={{height: '300px'}} onMouseEnter={this.handleMouseEnter}>
-        <HorizontalScroll >
-          {this.mapBeers()}
+        <div style={{height: '300px'}}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseMove={this.handleMouseMove}
+        >
+          <HorizontalScroll >
+            {this.mapBeers()}
           </HorizontalScroll>
         </div>
         </>
