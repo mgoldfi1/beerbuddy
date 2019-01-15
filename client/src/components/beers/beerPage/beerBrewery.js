8a87ff042d1910b446cfd91fb6a16caa9a8965f2
mgoldfi1 <mgoldfi1@gmail.com>
2019-01-14 18:33:45 -0800

    fixing merge conflicts

--- client/src/components/beers/beerPage/beerBrewery.js
+++ client/src/components/beers/beerPage/beerBrewery.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
 const BeerBrewery = ({brewery}) => {
-  const { name, website } = brewery
+  const { name, website, id } = brewery
   return (
         <>
-          <strong>Brewery Name:</strong> {name}<br/>
+          <strong>Brewery Name:</strong> <Link to={'/breweries/' + id}>{name}</Link><br/>
           <strong>Brewery Website:</strong>{' '}
           <a href={website} target="_blank">
             {website}
