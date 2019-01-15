0d93f5c945e4a65cdcbbcfc9df810b63d47bc39a
learn <eric.desimone@cortland.edu>
2019-01-15 13:27:17 -0500

    cleaned up brewery.js

--- client/src/components/breweries/breweryPage/brewery.js
+++ client/src/components/breweries/breweryPage/brewery.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Grid, Cell } from 'react-mdl';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
-import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom'
 import BreweryPanel from './breweryPanel'
@@ -18,22 +14,21 @@ const Brewery = (props) => {
       !!brewery ? (
           <Grid>
             <PageTitle title={brewery.name} colLength={12}/>
-            <div style={{width: '100vw', height: '10vh'}}/>
-              <Cell col={6}>
-                <BreweryPanel details={[brewery.region, brewery.country]} summary='Location'/>
-                <BreweryPanel details={brewery.description} summary='Description'/>
-                <BreweryPanel details={brewery.year} summary='Founded In'/>
-                <BreweryPanel
-                  details={<a href={brewery.website} target="_blank"> {brewery.website}</a>}
-                  summary='Brewery Website'
-                 />
-                <BreweryPanel
-                  details={brewery.openToPublic === "Y" ? "Yes":"No"}
-                  summary='Open to the Public?'
-                />
-              </Cell>
+            <Cell col={6}>
+              <BreweryPanel details={[brewery.region, brewery.country]} summary='Location'/>
+              <BreweryPanel details={brewery.description} summary='Description'/>
+              <BreweryPanel details={brewery.year} summary='Founded In'/>
+              <BreweryPanel
+                details={<a href={brewery.website} target="_blank"> {brewery.website}</a>}
+                summary='Brewery Website'
+               />
+              <BreweryPanel
+                details={brewery.openToPublic === "Y" ? "Yes":"No"}
+                summary='Open to the Public?'
+              />
+            </Cell>
             <Cell col={12}>
-            <div style={{fontSize: '16px'}}><strong>Beers made by this brewery:</strong></div>
+              <strong style={{fontSize: '16px'}}>Beers made by this brewery:</strong><br/>
                 {brewery.Beers.map(beer => {
                     return <div style={{display: "inline"}}><Link target="_blank" to={"/beer/" + beer.id}><img className="brewery-beers" src={beer.label}/></Link></div>
                 })}
@@ -47,5 +42,6 @@ Brewery.propTypes = {
   data: PropTypes.object.isRequired
 }
 
+  // <div style={{width: '100vw', height: '10vh'}}/>
 
 export default Brewery
