c76c02096104a3b13184394826adeef44ca707a6
learn <eric.desimone@cortland.edu>
2019-01-16 13:33:11 -0500

    moved auto scroll logic from brewery component to autoScroller

--- client/src/components/autoScroller/autoScroller.js
+++ client/src/components/autoScroller/autoScroller.js
@@ -3,9 +3,48 @@ import '../../css/autoScroller.css'
 
 class AutoScroller extends Component {
 
+  state = {
+    center: 0,
+    offset: 0,
+    scroller: 0,
+    visibility: 'hidden',
+    activate: ''
+  }
+
+  handleMouseEnter = (event) => {
+    let linkWidth = event.currentTarget.children[0].offsetWidth
+    let divWidth =  event.currentTarget.offsetWidth
+    let scroller = event.currentTarget
+    this.interval = setInterval(() => scroller.scrollBy(this.state.scroller, 0, 'smooth'), 50)
+    if (this.state.divWidth !== divWidth) {
+      this.setState({center: (divWidth/2), offset: (linkWidth)})
+    } else {
+    }
+  }
+
+  handleMouseLeave = () => {
+    clearInterval(this.interval)
+  }
+
+  handleMouseMove = (event) => {
+    let coord = event.clientX
+    let centerRange = [this.state.center - this.state.offset, this.state.center + this.state.offset]
+    if (coord <= centerRange[0]){
+        this.setState({scroller: (coord - centerRange[0]) * 0.1})
+    } else if (coord >= centerRange[1]) {
+        this.setState({scroller: (coord - centerRange[1]) * 0.1})
+    } else {
+      this.setState({scroller: 0})
+    }
+  }
+
     render() {
         return (
-          <div className='auto-scroller-container'>
+          <div className='auto-scroller-container'
+          onMouseEnter={this.handleMouseEnter}
+          onMouseLeave={this.handleMouseLeave}
+          onMouseMove={this.handleMouseMove}
+          >
             {this.props.children}
           </div>
         )
--- client/src/components/breweries/breweryPage/breweryBeers.js
+++ client/src/components/breweries/breweryPage/breweryBeers.js
@@ -7,53 +7,13 @@ import AutoScroller from '../../autoScroller/autoScroller'
 
 class BreweryBeers extends Component {
 
-  state = {
-    center: 0,
-    offset: 0,
-    scroller: 0,
-    visibility: 'hidden',
-    activate: ''
-  }
 
-  handleMouseEnter = (event) => {
-    let linkWidth = event.currentTarget.children[0].children[0].children[0].offsetWidth
-    let divWidth =  event.currentTarget.offsetWidth
-    let scroller = event.currentTarget.children[0]
-    this.interval = setInterval(() => scroller.scrollBy(this.state.scroller, 0, 'smooth'), 50)
-    if (this.state.divWidth !== divWidth) {
-      this.setState({center: (divWidth/2), offset: linkWidth, visibility: ''})
-    } else {
-      this.setState({visibility: ''})
-    }
-  }
-
-  handleMouseLeave = () => {
-    clearInterval(this.interval)
-    this.setState({visibility: 'hidden'})
-  }
-
-  handleMouseMove = (event) => {
-    let coord = event.clientX
-    let centerRange = [this.state.center - this.state.offset, this.state.center + this.state.offset]
-    if (coord <= centerRange[0]){
-        this.setState({scroller: (coord - centerRange[0]) * 0.1, activate: 'left'})
-    } else if (coord >= centerRange[1]) {
-        this.setState({scroller: (coord - centerRange[1]) * 0.1, activate: 'right'})
-    } else {
-      this.setState({scroller: 0, activate: ''})
-    }
-  }
 
 render(){
-  console.log(this.state.visibility)
     return (
         <>
         <strong style={{fontSize: '16px'}}>Beers made by this brewery:</strong><br/>
-        <div className="scroller-container"
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        onMouseMove={this.handleMouseMove}
-        >
+        <div className="scroller-container">
           <AutoScroller>
             <MapBeers beers={this.props.beers}/>
           </AutoScroller>
